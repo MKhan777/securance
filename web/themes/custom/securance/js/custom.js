85b78904daa7e0dbfa3f4e0e7ecc19556ed8dc56
MKhan777 <abdalli17b@gmail.com>
2024-08-20 18:52:25 +0500

    Update the advisory pages and configration of different components

--- web/themes/custom/securance/js/custom.js
+++ web/themes/custom/securance/js/custom.js
@@ -15,6 +15,13 @@
         wrapAround: true, // Optional: allow infinite scrolling
         pageDots: false,
       });
+      var flkty = new Flickity(".our-partners-block .flickity", {
+        cellAlign: "left", // Align cells to the left
+        contain: true, // Contain cells within the carousel
+        groupCells: 4, // Scroll one slide at a time
+        wrapAround: true, // Optional: allow infinite scrolling
+        pageDots: true,
+      });
     },
   };
 })(jQuery, Drupal);
